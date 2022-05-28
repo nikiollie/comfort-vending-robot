2d4cb03706e59213ca68e02dfa47f457ba2002fe
Hailey <hjames@ucsd.edu>
2022-05-27 17:24:57 -0700

    adding code for LED strip

--- arduino_files/Delivery_Blink/Delivery_Blink.ino
+++ arduino_files/Delivery_Blink/Delivery_Blink.ino
@@ -1,24 +1,38 @@
+#include <FastLED.h>
+
 char receivedChar;
 boolean newData = false;
 
-int RedPin = 3;
-int GreenPin = 5;
-int BluePin = 6;
+#define NUM_LEDS 60
+#define DATA_PIN 9
+CRGB leds[NUM_LEDS];
+
+//int RedPin = 3;
+//int GreenPin = 5;
+//int BluePin = 6;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(RedPin, OUTPUT);
-  pinMode(GreenPin, OUTPUT);
-  pinMode(BluePin, OUTPUT);
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+
+//  pinMode(RedPin, OUTPUT);
+//  pinMode(GreenPin, OUTPUT);
+//  pinMode(BluePin, OUTPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(RedPin, 0);
-  analogWrite(GreenPin, 0);
-  analogWrite(BluePin, 255);
+//  analogWrite(RedPin, 0);
+//  analogWrite(GreenPin, 0);
+//  analogWrite(BluePin, 255);
+
+  for( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Blue;
+  }
+  FastLED.show();
+  
   recvOneChar();
   showNewData();
 }
@@ -34,9 +48,15 @@ void recvOneChar() {
 void showNewData() {
   if (newData== true) {
     Serial.println(receivedChar);
-    analogWrite(RedPin, 0);
-    analogWrite(BluePin, 0);
-    analogWrite(GreenPin, 255);
+//    analogWrite(RedPin, 0);
+//    analogWrite(BluePin, 0);
+//    analogWrite(GreenPin, 255);
+
+    for( int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Green;
+    }
+    FastLED.show();
+    
     delay(1000);
     newData= false;
   }
